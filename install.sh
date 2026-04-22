2c465458edd8af1efd8cebec5d969b97828fd248
SoulArcher0507 <enea.ares2004@gmail.com>
2026-04-22 20:03:52 +0200

    better install

--- install.sh
+++ install.sh
@@ -90,15 +90,18 @@ select LABEL in "${THEME_LABELS[@]}"; do
     echo ""
 done
 
-sudo bash "$SCRIPT_DIR/Resources/Grub/grubsouls-theme/install_theme.sh"
+bash "$SCRIPT_DIR/Resources/Grub/grubsouls-theme/install_theme.sh"
 bash "$SCRIPT_DIR/Resources/Scripts/install_sddm.sh"
-sudo bash "$SCRIPT_DIR/Resources/Scripts/change_sddm_avatar.sh" "$TARGET_USER" "$SCRIPT_DIR/Resources/Wallpapers/shadow_army.jpg"
-sudo bash "$SCRIPT_DIR/Resources/Scripts/install_plymouth_theme.sh" "colorful_loop" "$SCRIPT_DIR/Resources/Plymouth/plymouth-themes"
+bash "$SCRIPT_DIR/Resources/Scripts/change_sddm_avatar.sh" "$TARGET_USER" "$SCRIPT_DIR/Resources/Wallpapers/shadow_army.jpg"
+bash "$SCRIPT_DIR/Resources/Scripts/install_plymouth_theme.sh" "colorful_loop" "$SCRIPT_DIR/Resources/Plymouth/plymouth-themes"
 run_target_bash "$SCRIPT_DIR/Resources/Scripts/zsh_enabler.sh"
-sudo bash "$SCRIPT_DIR/Resources/Scripts/dynamic_swap_file.sh"
+bash "$SCRIPT_DIR/Resources/Scripts/dynamic_swap_file.sh"
 run_target_bash "$SCRIPT_DIR/Resources/Scripts/default_webapps.sh"
 
 run_target_cmd mkdir -p "$TARGET_HOME/Pictures/Wallpapers" "$TARGET_HOME/Pictures/Icons"
 run_target_cmd rsync -av --progress "$SCRIPT_DIR/Resources/Wallpapers/" "$TARGET_HOME/Pictures/Wallpapers/"
 run_target_cmd rsync -av --progress "$SCRIPT_DIR/Resources/Icons/" "$TARGET_HOME/Pictures/Icons/"
 
+$TARGET_HOME/.config/awww/wallpaper.sh "$TARGET_HOME/Pictures/Wallpapers/shadow_army.jpg"
+
+echo "=== END OF INSTALLATION ==="
